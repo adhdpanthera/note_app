a26fb1ac81c30ff7a4fea76042f45ca0bd788076
Yana Safina <adhdpanthera@gmail.com>
2022-05-08 16:51:57 +0300

    Причесала код

--- app/src/main/java/com/example/note_app/NoteEditorActivity.java
+++ app/src/main/java/com/example/note_app/NoteEditorActivity.java
@@ -20,10 +20,8 @@ public class NoteEditorActivity extends AppCompatActivity {
 
         EditText editText = findViewById(R.id.editText);
 
-        // Fetch data that is passed from MainActivity
         Intent intent = getIntent();
 
-        // Accessing the data using key and value
         noteId = intent.getIntExtra("noteId", -1);
         if (noteId != -1) {
             editText.setText(MainActivity.notes.get(noteId));
@@ -38,14 +36,14 @@ public class NoteEditorActivity extends AppCompatActivity {
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                // add your code here
+                //
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 MainActivity.notes.set(noteId, String.valueOf(charSequence));
                 MainActivity.arrayAdapter.notifyDataSetChanged();
-                // Creating Object of SharedPreferences to store data in the phone
+                // Делаем сохранение заметок в памяти телефона
                 SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
                 HashSet<String> set = new HashSet(MainActivity.notes);
                 sharedPreferences.edit().putStringSet("notes", set).apply();
@@ -53,7 +51,7 @@ public class NoteEditorActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                // add your code here
+                //
             }
         });
     }
